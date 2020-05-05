571c9efac7659b7750c53fe830e5d799b4ba5e51
Oguzhan <oguzhanturgut@outlook.com>
2020-05-05 15:26:33 +0200

    add tasks module, controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
 })
 export class AppModule {}
